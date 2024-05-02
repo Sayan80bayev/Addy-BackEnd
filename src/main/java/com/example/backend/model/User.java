e598271825f7e022115427ec76a100997c580df2
Sayan80bayev <sayan123serv@gmail.com>
2024-05-02 19:57:54 +0500

    docker update

--- src/main/java/com/example/backend/model/User.java
+++ src/main/java/com/example/backend/model/User.java
@@ -3,17 +3,9 @@ package com.example.backend.model;
 import java.util.Collection;
 import java.util.List;
 
+import jakarta.persistence.*;
 import org.springframework.security.core.userdetails.UserDetails;
 
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -28,9 +20,10 @@ import com.example.backend.enums.*;
 @Builder
 @Entity
 @Data
+@Table(name = "users")
 public class User implements UserDetails {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
