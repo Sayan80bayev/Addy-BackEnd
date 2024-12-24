3a6ea5ee6a4518fce99fec4fec00f599189f5e75
sayan80bayev <sayan123serv@gmail.com>
2024-12-24 22:39:55 +0500

    Rewised CategorySpecification, has been added search by parent category with their child

--- src/main/java/com/example/backend/filter/impl/CategorySpecification.java
+++ src/main/java/com/example/backend/filter/impl/CategorySpecification.java
@@ -3,17 +3,28 @@ package com.example.backend.filter.impl;
 import com.example.backend.dto.request.AdvertisementFilterRequest;
 import com.example.backend.filter.FilterSpecification;
 import com.example.backend.model.Advertisement;
+import jakarta.persistence.criteria.Join;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Component;
 
-import java.util.UUID;
-
 @Component
 public class CategorySpecification implements FilterSpecification<Advertisement> {
+
     @Override
     public Specification<Advertisement> toSpecification(AdvertisementFilterRequest advertisementFilterRequest) {
-        return (root, query, criteriaBuilder) ->
-                criteriaBuilder.equal(root.get("category").get("id"), advertisementFilterRequest.getCategoryId());
+        return (root, query, criteriaBuilder) -> {
+            // Get the category ID from the filter request
+            var categoryId = advertisementFilterRequest.getCategoryId();
+
+            // Join the category table
+            Join<Object, Object> categoryJoin = root.join("category");
+
+            // Check for matching category ID or its parent's ID
+            return criteriaBuilder.or(
+                    criteriaBuilder.equal(categoryJoin.get("id"), categoryId),
+                    criteriaBuilder.equal(categoryJoin.get("parent").get("id"), categoryId)
+            );
+        };
     }
 
     @Override
